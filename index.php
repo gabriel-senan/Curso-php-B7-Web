758d6e55754f6e69e1d35cf24ca28b75cd83581d
gabriel-senan <gabrielsnnh50@gmail.com>
2024-07-24 20:56:50 -0300

    algumas

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
 <?php
-$ingredientes = [
-    'açúcar' => "doce",
-    'farinha de trigo',
-    'ovos',
-    'leite',
-    'ferment em pó'
-];
-
-$numerosDaMegaSena = [12, 5, 3, 18, 22, 17];
-
-echo "Ingrediente selecionado: ".$ingredientes[0]. "</br>";
-
-echo "Número selecionado: ". $numerosDaMegaSena[0];
