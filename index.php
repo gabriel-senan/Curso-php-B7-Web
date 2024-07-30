cbea37fef204fecab95f5eda02f5d572df914775
gabriel-senan <gabrielsnnh50@gmail.com>
2024-07-29 22:38:40 -0300

    #f0018

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
 
-$numero = 0;
-
 /*  Loop While
+$numero = 0;
 while($numero <= 10) {
     echo "N: ".$numero."\n";
 
     $numero += 1;
 }   */
 
-for($numero = 0; $numero < 10; $numero+= 1) {
+ /* for($numero = 0; $numero < 10; $numero+= 1) {
     echo "N: ".$numero."\n";
+} */
+
+$ingredientes = [
+    'Açúcar',
+    'Farinha de trigo',
+    'Leite',
+    'Fermento em pó'
+];
+foreach($ingredientes as $chave => $valor) {
+    echo "Item: ".($chave + 1).": ".$valor."\n";
 }
