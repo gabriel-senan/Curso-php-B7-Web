72398a563b6cfa9dec823f6cbe8abaeb18ebe111
gabriel-senan <gabrielsnnh50@gmail.com>
2024-07-29 22:07:18 -0300

    #f0017

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
+
 $numero = 0;
 
+/*  Loop While
 while($numero <= 10) {
     echo "N: ".$numero."\n";
 
     $numero += 1;
+}   */
+
+for($numero = 0; $numero < 10; $numero+= 1) {
+    echo "N: ".$numero."\n";
 }
