4cedf56962bbcc1bc0c6b7830b50097c2e2ef02d
gabriel-senan <gabrielsnnh50@gmail.com>
2024-07-30 20:25:44 -0300

    #f019

---
+++ exercicio.php
@@ -0,0 +1,14 @@
+<?php
+$traco = "-";
+
+// Com isso desenhamos 10 linhas na horizontal
+for ($i = 0; $i < 10; $i++) {
+    // Para cada linha na horizontal, aparece 10 traços
+    for ($j = 0; $j < 10; $j++) {
+        // Desenhar o traço
+        echo $traco;
+    }
+    
+    echo "\n";
+}
+?>
