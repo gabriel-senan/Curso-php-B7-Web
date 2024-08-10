8bb3ab93b815a32ac3bb308cc07b5b6d1242fc5c
gabriel-senan <gabrielsnnh50@gmail.com>
2024-08-10 12:16:13 -0300

    #F022

---
+++ 022.php
@@ -0,0 +1,17 @@
+<?php
+function subsequente() {
+    for ($q = 0; $q < 10; $q++) {
+        echo $q . "<br/>";
+    }
+
+    echo "<hr/>";
+
+}
+
+subsequente();
+
+function latir() {
+    echo "au au";
+}
+
+latir();
