57595c4f9712650d0234a1dcff42801d965c25c9
gabriel-senan <gabrielsnnh50@gmail.com>
2024-08-21 20:54:32 -0300

    030

--- 030.php
+++ 030.php
@@ -7,3 +7,12 @@ $nomeLimpo = trim($nomeSujo);
 echo "NOME SUJO: " . strlen($nomeSujo) . "\n";
 echo "NOME LIMPO: " . strlen($nomeLimpo) . "\n"; */
 
+/*
+$nome = "Gabriel Sena";
+
+echo strtolower($nome) . "\n";
+echo strtoupper($nome) . "\n";
+
+$nomeAlterado = str_replace("a", "4", $nome);
+echo $nomeAlterado; /*
+
