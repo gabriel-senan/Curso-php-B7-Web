31eddc1510e1a1f69e4f5d8b421cc9703eadcd41
gabriel-senan <gabrielsnnh50@gmail.com>
2024-08-20 21:24:29 -0300

    funcoes matematicas

---
+++ 029.php
@@ -0,0 +1,26 @@
+<?php
+/*
+//Exibe o numero absoluto
+$numero = -8.4;
+echo abs($numero);*/
+
+/*
+//Exibe o valor de pi
+echo pi(); */
+
+/*
+//arredonda sempre para baixo/ baixo
+$numero = 2.7;
+echo floor($numero) . "\n";
+echo round($numero) . "\n";
+echo ceil($numero) .  "\n"; */
+
+/*
+//Gera numeros aleatorios
+$aleatorio = rand(0, 6);
+echo ($aleatorio); */
+
+
+$lista = [1, 2, 3];
+echo max($lista) . "\n";
+echo min($lista) . "\n";
