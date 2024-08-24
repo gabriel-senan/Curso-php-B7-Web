bfe3c431af8447f7187a6d19e0315b2a3b533f6c
gabriel-senan <gabrielsnnh50@gmail.com>
2024-08-24 20:31:33 -0300

    in array

--- 033.php
+++ 033.php
@@ -6,8 +6,17 @@ $numeros = [10, 20, 24, 91, 18];
 array_pop($numeros);
 print_r($numeros); */
 
-
+/*
 $numeros = [10, 20, 24, 91, 18];
 
 array_shift($numeros);
-print_r($numeros);
+print_r($numeros); */
+
+
+$numeros = [10, 20, 24, 91, 18];
+
+if (in_array(91, $numeros)) {
+    echo "EXISTE!!";
+} else {
+    echo "NÃO EXISTE!!";
+}
