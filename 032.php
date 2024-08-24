a6efe048ea57e56c8bc41458d736f0021ccfe9c1
gabriel-senan <gabrielsnnh50@gmail.com>
2024-08-24 20:16:22 -0300

    filtro de array

--- 032.php
+++ 032.php
@@ -1,4 +1,25 @@
 <?php
-
+/*
 $lista = ["nome1", "nome2", "nome3", "nome4", "nome5"];
-echo "Total: " . count($lista) . "\n";
+echo "Total: " . count($lista) . "\n"; */
+
+/*
+$lista = ["gabriel", "victor", "thamires", "branca", "luis", "thobias"];
+$aprovados = ["gabriel", "victor", "thamires"];
+
+$reprovados = array_diff($lista, $aprovados);
+
+print_r($reprovados) . "\n"; */
+
+/*
+$numeros = [10, 20, 24, 91, 18];
+$filtrados = array_filter($numeros, function($item){
+    if($item < 30) {
+        return true;
+    } else {
+        return false;
+    }
+});
+
+print_r($filtrados); */
+
