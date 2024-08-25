8370583c4a12e6e7202004f4d276c6aee3622bfc
gabriel-senan <gabrielsnnh50@gmail.com>
2024-08-25 11:42:13 -0300

    data

--- 034.php
+++ 034.php
@@ -5,5 +5,10 @@ echo time(); */
 /*
 echo date("d/m/Y H:i:s"); */
 
+/*
 $data = '2020-03-07';
+$time = strtotime($data);
+ echo date("d/m/Y", $time); */
 
+ $data = '2020-03-07';
+ echo date("d/m/Y", strtotime($data)); 
