b8cc9ebc25ee9afac4fe451564fb15cdeebc4754
gabriel-senan <gabrielsnnh50@gmail.com>
2024-08-31 15:09:47 -0300

    add

--- 038_index.php
+++ 038_index.php
@@ -1,1 +1,21 @@
 <?php
+require("038_header.php");
+?>
+
+<form method="POST">
+
+    <label>
+        Nome:
+        <br />
+        <input type="text" name="nome" />
+    </label>
+    <br />
+    <br />
+    <label>
+        Idade:
+        <br />
+        <input type="text" name="idade" />
+    </label>
+    <br />
+    <br />
+</form>
