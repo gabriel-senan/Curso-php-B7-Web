2eebb0469e8523dd25cfb506f64e3a974e25f80e
gabriel-senan <gabrielsnnh50@gmail.com>
2024-09-24 22:25:43 -0300

    adddd

--- 038_recebedor.php
+++ 038_recebedor.php
@@ -3,6 +3,7 @@ $nome = filter_input(INPUT_POST, "nome");
 $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
 $idade = filter_input(INPUT_POST, "idade", FILTER_SANITIZE_NUMBER_INT);
 
+
 if($nome && $email && $idade) {
     echo 'NOME: ' . $nome . "<br/>";
     echo 'EMAIL: ' . $email . "<br/>";
